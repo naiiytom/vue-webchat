0454d58232a7ce13f629740e7d470ee3774ec275
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2021-02-21 10:12:30 +0000

    implement chat widget

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import Chat from "vue-beautiful-chat";
 
 Vue.config.productionTip = false;
+Vue.use(Chat);
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount("#app");
